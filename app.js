8b5e787b535e9ff3d026a05ca7324c1b1c03929d
Lithira-Dilitha <lithiradilitha2022@gmail.com>
2024-04-29 21:44:06 +0530

    change css Styles

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ function currentcity() {
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
             let currentdata = ` <div>
                                 <h1><img width="35" height="35" src="https://img.icons8.com/color/48/place-marker--v2.png" alt="place-marker--v2"/>${data.location.name}</h1>
                                 &nbsp;&nbsp;&nbsp;<img src="${data.current.condition.icon}" alt="">
@@ -20,16 +19,17 @@ function currentcity() {
             currentWeather.innerHTML = currentdata;
 
             let body =`
-                        <div class="mt-3">
+                        <div class="anotherdata mt-3">
                         <p><img width="35" height="35" src="https://img.icons8.com/color/48/country.png" alt="country"/><b> Country :</b>&nbsp;&nbsp;&nbsp;${data.location.country}</p>
-                        <p><img width="35" height="35" src="https://img.icons8.com/color/48/hygrometer.png" alt="hygrometer"/><b> Humidity :</b>&nbsp;&nbsp;${data.current.humidity}</p>
-                        <p><img width="35" height="35" src="https://img.icons8.com/fluency/48/cloud.png" alt="cloud"/><b> Cloud :</b>&nbsp;&nbsp;${data.current.cloud}</p>
+                        <p><img width="35" height="35" src="https://img.icons8.com/color/48/hygrometer.png" alt="hygrometer"/><b> Humidity :</b>&nbsp;&nbsp;${data.current.humidity}&percnt;</p>
+                        <p><img width="35" height="35" src="https://img.icons8.com/fluency/48/cloud.png" alt="cloud"/><b> Cloud :</b>&nbsp;&nbsp;${data.current.cloud}&percnt;</p>
                         <p><img width="35" height="35" src="https://img.icons8.com/color/48/wind.png" alt="wind"/><b> Wind speed :</b><td>&nbsp;&nbsp;${data.current.wind_kph} km/h</p>
                         <p><img width="48" height="48" src="https://img.icons8.com/color/48/do-not-expose-to-sunlight.png" alt="do-not-expose-to-sunlight"/><b> Uv :</b><td>&nbsp;&nbsp;${data.current.uv}</td></p>
 
                     </div>`
             
                     anotherdata.innerHTML=body;
+
             let forcastData="";
             function getForcast(ForcastArray) {
                 for (var i = 0; i < ForcastArray.length; i++) {
@@ -38,9 +38,12 @@ function currentcity() {
                                         <h3>${ForcastArray[i].date}</h3>
                                         <div>
                                             <img src="${ForcastArray[i].day.condition.icon}" alt="">
-                                            <p>${ForcastArray[i].day.condition.text}</p>
+                                            <p><b>${ForcastArray[i].day.condition.text}</b></p>
                                         </div>
-                                        <h2><img width="35" height="35" src="https://img.icons8.com/fluency/48/cloud.png" alt="cloud"/>${ForcastArray[i].day.avgtemp_c}</h2>
+                                        <p><img width="30" height="30" src="https://img.icons8.com/color/48/temperature--v2.png" alt="temperature--v2"/>&nbsp;<b>temperature :</b>&nbsp;&nbsp;${ForcastArray[i].day.avgtemp_c}&#8451;</p>
+                                        <p><img width="30" height="30" src="https://img.icons8.com/color/48/wind.png" alt="wind"/>&nbsp;<b>wind speed :</b>&nbsp;&nbsp;${ForcastArray[i].day.maxwind_mph} km/h</p>
+                                        <p><img width="30" height="30" src="https://img.icons8.com/color/48/hygrometer.png" alt="hygrometer"/>&nbsp;<b>Average humidity :</b>&nbsp;&nbsp;${ForcastArray[i].day.avghumidity}&percnt;</p>
+                                        <p><img width="30" height="30" src="https://img.icons8.com/color/48/rain--v1.png" alt="rain--v1"/>&nbsp;<b>Chance Of Rain :</b>&nbsp;&nbsp;${ForcastArray[i].day.daily_chance_of_rain}&percnt;</p>
                                     </div>
                                     `
                 };
@@ -67,44 +70,37 @@ function getAnotherCity() {
                                 </div>`
             currentWeather.innerHTML = currentdata;
 
-            let body = `<tr>
-                        <td><img width="35" height="35" src="https://img.icons8.com/color/48/country.png" alt="country"/><b> Country</b></td>
-                        <td>${data.location.country}</td>
-                    </tr>
-                    <tr>
-                        <td scope="col"><img width="35" height="35" src="https://img.icons8.com/color/48/hygrometer.png" alt="hygrometer"/><b> Humidity</b></td>
-                        <td>${data.current.humidity}</td>
-                    </tr>
-                    <tr>
-                        <td scope="col"><img width="35" height="35" src="https://img.icons8.com/fluency/48/cloud.png" alt="cloud"/><b> Cloud</b></td>
-                        <td>${data.current.cloud}</td>
-                    </tr>
-                    <tr>
-                        <td><img width="35" height="35" src="https://img.icons8.com/color/48/wind.png" alt="wind"/><b> Wind speed</b></td>
-                        <td>${data.current.wind_kph} km/h</td>
-                    </tr>
-                    <tr>
-                        <td><img width="48" height="48" src="https://img.icons8.com/color/48/do-not-expose-to-sunlight.png" alt="do-not-expose-to-sunlight"/><b> Uv</b></td>
-                        <td>${data.current.uv}</td>
-                    </tr>`
-            tblanotherData.innerHTML = body;
+            let body =`
+                        <div class="mt-3" style="position: relative; left: 25px;">
+                        <p><img width="35" height="35" src="https://img.icons8.com/color/48/country.png" alt="country"/><b> Country :</b>&nbsp;&nbsp;&nbsp;${data.location.country}</p>
+                        <p><img width="35" height="35" src="https://img.icons8.com/color/48/hygrometer.png" alt="hygrometer"/><b> Humidity :</b>&nbsp;&nbsp;${data.current.humidity}&percnt;</p>
+                        <p><img width="35" height="35" src="https://img.icons8.com/fluency/48/cloud.png" alt="cloud"/><b> Cloud :</b>&nbsp;&nbsp;${data.current.cloud}&percnt;<span></span></p>
+                        <p><img width="35" height="35" src="https://img.icons8.com/color/48/wind.png" alt="wind"/><b> Wind speed :</b><td>&nbsp;&nbsp;${data.current.wind_kph} km/h</p>
+                        <p><img width="48" height="48" src="https://img.icons8.com/color/48/do-not-expose-to-sunlight.png" alt="do-not-expose-to-sunlight"/><b> Uv :</b><td>&nbsp;&nbsp;${data.current.uv}</td></p>
+
+                    </div>`
+            
+                    anotherdata.innerHTML=body;
 
             let forcastData="";
             function getForcast(ForcastArray) {
                 for (var i = 0; i < ForcastArray.length; i++) {
-                    console.log(ForcastArray[i].date);
-                    console.log(ForcastArray[i].day.condition.icon);
-                    forcastData += `<tr>
-                                    <td><img src="${ForcastArray[i].day.condition.icon}" alt=""></td>
-                                     <td>${ForcastArray[i].date}</td>
-                                     <td>${ForcastArray[i].day.condition.text}</td>
-                                     <td>${ForcastArray[i].day.avgtemp_c}</td>
-                                     <td>${ForcastArray[i].day.maxwind_mph}</td>
-                                     <td>${ForcastArray[i].day.avghumidity}</td>
-                                </tr>`
+                    forcastData +=`
+                                    <div class="transperent col-lg-3  col-sm-12 mt-sm-5 ms-lg-5">
+                                        <h3>${ForcastArray[i].date}</h3>
+                                        <div>
+                                            <img src="${ForcastArray[i].day.condition.icon}" alt="">
+                                            <p><b>${ForcastArray[i].day.condition.text}</b></p>
+                                        </div>
+                                        <p><img width="30" height="30" src="https://img.icons8.com/color/48/temperature--v2.png" alt="temperature--v2"/>&nbsp;<b>temperature :</b>&nbsp;&nbsp;${ForcastArray[i].day.avgtemp_c}&#8451;</p>
+                                        <p><img width="30" height="30" src="https://img.icons8.com/color/48/wind.png" alt="wind"/>&nbsp;<b>wind speed :</b>&nbsp;&nbsp;${ForcastArray[i].day.maxwind_mph} km/h</p>
+                                        <p><img width="30" height="30" src="https://img.icons8.com/color/48/hygrometer.png" alt="hygrometer"/>&nbsp;<b>Average humidity :</b>&nbsp;&nbsp;${ForcastArray[i].day.avghumidity}&percnt;</p>
+                                        <p><img width="30" height="30" src="https://img.icons8.com/color/48/rain--v1.png" alt="rain--v1"/>&nbsp;<b>Chance Of Rain :</b>&nbsp;&nbsp;${ForcastArray[i].day.daily_chance_of_rain}&percnt;</p>
+                                    </div>
+                                    `
                 };
-                tblforcastData.innerHTML = forcastData;
+                forcastdataRow.innerHTML=forcastData;
             }
-            getForcast(data.forecast.forecastday);
+            getForcast(data.forecast.forecastday); 
         })
 }
